005dd34b337bbaa093366901032ae15bc0ab1997
Fabian Köster <mail@fabian-koester.com>
2016-11-21 13:45:50 +0100

    Copy config

--- gen_deb_package.sh
+++ gen_deb_package.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 npm install
-node_modules/.bin/node-deb -- node_modules index.js sensors.js LICENSE package.json
+node_modules/.bin/node-deb -- node_modules index.js sensors.js sensorconfig_example.json LICENSE package.json

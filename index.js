de3353ee963f7bb223ccf282e6d9fa870380ea9e
Fabian Köster <mail@fabian-koester.com>
2016-11-27 13:44:34 +0100

    Allow Cross-Origin Access

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 const client = require('prom-client');
 const express = require('express');
+const cors = require('cors');
 require('./sensors');
 
 const server = express();
+
+server.use(cors());
 
 server.get('/metrics', (req, res) => {
   res.header('Content-Type', 'text/plain; version=0.0.4; charset=utf-8');
